d8181b6b360e8fcf11672e6386b8f973dc0bfc18
crito <crito@flakispace.org>
2020-03-30 22:43:29 +0000

    implement database cloning and fix bugs
    * redirect output to journald
    * silence extractor
    * silence pushd/popd
    * replace hardcoded flavor tbc with variable
    
    TODO:
    * remove cmake_install_path workaround

--- cmangos/cmangos-update.sh
+++ cmangos/cmangos-update.sh
@@ -1,13 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+clean_db() {
+  TABLES=$(mysql -h ${1} -u ${2} -p${3} ${4} -s -r -e "show tables;" 2> /dev/null)
+  for t in ${TABLES}; do
+    mysql -h ${1} -u ${2} -p${3} ${4} -s -r -e "drop table ${t};" 2>&1
+  done
+}
 
 echo_h1() {
   echo -e "\033[0;32m::\033[0m ${1}\033[0m"
 }
 
 echo_h2() {
-  echo -e "\033[0;33m->\033[0;37m ${1}\033[0m"
+  echo -e "\033[0;33m ->\033[0;37m ${1}\033[0m"
 }
 
 echo_h3() {
@@ -15,7 +22,7 @@ echo_h3() {
 }
 
 get_branches() {
-  BRANCHES=$(git -C $1 branch -r --color=never | grep tbc | sed 's/\*//' | awk '{ print $1 }')
+  BRANCHES=$(git -C $1 branch -r --color=never | grep $2 | sed 's/\*//' | awk '{ print $1 }')
   for b in $BRANCHES; do
     echo $(echo $b | sed 's_origin/__')
   done
@@ -23,7 +30,7 @@ get_branches() {
 
 get_db_version() {
   mysql -h ${1} -u ${2} -p${3} ${4} \
-    -s -r -e "show create table ${5};" | \
+    -s -r -e "show create table ${5};" 2> /dev/null | \
     grep required | \
     awk '{ print $1 }' | \
     sed 's/`required_\(.*\)`/\1/'
@@ -68,39 +75,43 @@ if [ ! -f /etc/cmangos-update/${FLAVOR}.conf ]; then
   echo "missing mysql config (/etc/cmangos-update/${FLAVOR}.conf)"
   exit 3
 fi
-source /etc/cmangos-update/${FLAVOR}.conf
+. /etc/cmangos-update/${FLAVOR}.conf
 
 for repo in ${REPOS}; do
   S="/usr/src/${repo}"
-  B="${S}/build"
 
   echo_h1 "update repo in \033[0;34m${S}"
   echo_h2 "fetch master"
   git -C $S checkout master &> /dev/null
-  git -C $S pull upstream master
-  git -C $S checkout -D ptr/$BUILD &> /dev/null || true
+  git -C $S pull upstream master | systemd-cat -t ${JOURNALD_TARGET}
+  git -C $S branch -D ptr/$BUILD &> /dev/null || true
   git -C $S checkout -b ptr/$BUILD &> /dev/null
 
-  for branch in $(get_branches $S); do
+  for branch in $(get_branches $S ${FLAVOR}); do
+    # workaround
+    if [ "${branch}" = "tbc/bugfix/cmake_install_path" ]; then
+      continue
+    fi
     echo_h2 "rebase \033[0;33m${branch}"
     git -C $S branch -D $branch &> /dev/null || true
-    git -C $S fetch origin $branch
-    git -C $S checkout $branch
-    git -C $S rebase master
+    git -C $S fetch origin $branch | systemd-cat -t ${JOURNALD_TARGET}
+    git -C $S checkout $branch | systemd-cat -t ${JOURNALD_TARGET}
+    git -C $S rebase master | systemd-cat -t ${JOURNALD_TARGET}
 
     echo_h2 "apply feature/bug \033[0;33m${branch}"
     git -C $S checkout ptr/$BUILD &> /dev/null
-    git -C $S rebase $branch
+    git -C $S rebase $branch | systemd-cat -t ${JOURNALD_TARGET}
   done
 done
 
 echo_h1 "build core and client files"
 S="/usr/src/cmangos-${FLAVOR}"
+B="${S}/build"
 D="/opt/cmangos-${FLAVOR}-${BUILD}"
 rm -rf $B; install -d $B
 
 echo_h2 "prepare build dir"
-pushd $B
+pushd $B &> /dev/null
 cmake .. \
   -DBUILD_PLAYERBOT=1 \
   -DBUILD_EXTRACTORS=1 \
@@ -114,28 +125,44 @@ make -j${CORES} | systemd-cat -t ${JOURNALD_TARGET}
 
 echo_h2 "install core"
 make install | systemd-cat -t ${JOURNALD_TARGET}
-popd
+popd &> /dev/null
 
 echo_h2 "extract client files"
-pushd $B/bin/tools
+pushd $D/bin/tools &> /dev/null
 chmod u+x ExtractResources.sh MoveMapGen.sh
 install -d /usr/share/cmangos/${CVER}-${BUILD}
 
 if [ "${FLAVOR}" = "tbc" ]; then
   ./ExtractResources.sh a \
     /opt/clients/WoW-${CVER}-MultiLoc \
-    /usr/share/cmangos/${CVER}-${BUILD} | systemd-cat -t ${JOURNALD_TARGET}
+    /usr/share/cmangos/${CVER}-${BUILD} <<EOF | systemd-cat -t ${JOURNALD_TARGET}
+${CORES}
+y
+y
+EOF
 fi
 
-popd
+popd &> /dev/null
 
 echo_h1 "update world server"
-systemctl stop cmangos-${FLAVOR}
-DBVER=$(get_db_version ${SQLHOST} ${SQLUSER} ${SQLPW} ${SQLDB} character_db_version)
+systemctl stop cmangos-${FLAVOR} || true
+sleep 60
+
+if [ "$SQLSYNC" = "1" ]; then
+  echo_h2 "clone template database"
+  clean_db ${SQLHOST} ${SQLUSER} ${SQLPW} ${SQLDBWORLD} | systemd-cat -t ${JOURNALD_TARGET}
+  clean_db ${SQLHOST} ${SQLUSER} ${SQLPW} ${SQLDBCHARS} | systemd-cat -t ${JOURNALD_TARGET}
+  mysqldump -h ${SYNCHOST} -u ${SYNCUSER} -p${SYNCPW} ${SYNCDBWORLD} 2> /dev/null | \
+    mysql -h ${SQLHOST} -u ${SQLUSER} -p${SQLPW} ${SQLDBWORLD} 2>&1 | systemd-cat -t ${JOURNALD_TARGET}
+  mysqldump -h ${SYNCHOST} -u ${SYNCUSER} -p${SYNCPW} ${SYNCDBCHARS} 2> /dev/null | \
+    mysql -h ${SQLHOST} -u ${SQLUSER} -p${SQLPW} ${SQLDBCHARS} 2>&1 | systemd-cat -t ${JOURNALD_TARGET}
+fi
+
+DBVER=$(get_db_version ${SQLHOST} ${SQLUSER} ${SQLPW} ${SQLDBCHARS} character_db_version)
 echo_h2 "update char database from version \033[0;33m${DBVER}"
 dbmatch=0
 
-pushd $S/sql/updates/characters
+pushd $S/sql/updates/characters &> /dev/null
 for f in $(ls -1 *.sql | awk -F'.' '{ print $1 }'); do
   if [ $dbmatch -eq 0 ]; then
     if [ "$f" == "${DBVER}" ]; then
@@ -145,13 +172,17 @@ for f in $(ls -1 *.sql | awk -F'.' '{ print $1 }'); do
     fi
   else
     echo_h3 "apply char db update \033[0;33m${f}"
-    mysql -h ${SQLHOST} -u ${SQLUSER} -p${SQLPW} ${SQLDB} < ${f}.sql
+    mysql -h ${SQLHOST} -u ${SQLUSER} -p${SQLPW} ${SQLDB} < ${f}.sql 2>&1 | systemd-cat -t ${JOURNALD_TARGET}
   fi
 done
-popd
+popd &> /dev/null
 
 echo_h2 "apply world db updates"
 S="/usr/src/cmangos-${FLAVOR}-db"
-pushd ${S}
+pushd ${S} &> /dev/null
 ./InstallFullDB.sh | systemd-cat -t ${JOURNALD_TARGET}
-popd
+popd &> /dev/null
+
+ln -snf /opt/cmangos-${FLAVOR}-${BUILD} /opt/cmangos-${FLAVOR}
+ln -snf /usr/share/cmangos/${CVER}-${BUILD} /usr/share/cmangos/${CVER}
+systemctl start cmangos-${FLAVOR}
